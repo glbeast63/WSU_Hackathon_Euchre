944e544601689ff823ce3702ec81c2154b9f49be
Charlie <glbeast63@yahoo.com>
2016-05-22 09:45:01 -0400

    sdasd
    sadsd

---
+++ SignalRMVC/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.IO;
+
+namespace SignalRMVC.Hubs
+{
+	public class ChatHub : Hub
+	{
+		public void Send(string name, string message)
+		{
+			// Call the addNewMessageToPage method to update clients.
+			Clients.All.addNewMessageToPage(name, message);
+			File.AppendAllText("C:\\Users\\colli_000\\Desktop\\test.txt", name);
+		}
+	}
+}
---
+++ SignalRMVC/Hubs/GameHub.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.IO;
+
+namespace SignalRMVC.Hubs
+{
+	public class GameHub : Hub
+	{
+		public void Send(string name)
+		{
+			// Call the test method to update clients.
+			Clients.All.test(name);
+            //File.AppendAllText("C:\\Users\\colli_000\\Desktop\\test.txt", name);
+        }
+
+        public void Loaner(string name)
+        {
+
+        }
+
+        public void Pass(string name)
+        {
+
+        }
+
+        public void Pickup(string name)
+        {
+
+        }
+    }
+}
